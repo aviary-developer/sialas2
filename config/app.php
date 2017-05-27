34b7601b9ce32f6a7e27c378b120c04567267855
René Ruiz <rne.eclipse@gmail.com>
2017-05-26 21:31:58 -0600

    Configuracion
    La configuración de las librerías a utlizar: DOMPDF, Laravel Collective
    y Doctrine / DBAL

--- config/app.php
+++ config/app.php
@@ -176,6 +176,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        //Librerías a utilizar
+        Collective\Html\HtmlServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
 
     ],
 
@@ -225,6 +228,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
+
 
     ],
 
